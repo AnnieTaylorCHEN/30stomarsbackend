1b84c3a2beeac04ab85c6bb3c59e4a0721fb3019
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-08-09 15:21:16 +0200

    fix header issue.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
 	next();
 });
 
