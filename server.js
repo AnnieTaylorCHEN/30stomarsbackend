21a91c3aa17709ca9dc3d022627db094619325b5
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-08-09 15:27:43 +0200

    allow x-auth-token.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH');
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token'
 	);
 	next();
 });
